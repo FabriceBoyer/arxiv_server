5eb791cc38096b35095886181f386186b123f3de
Fabrice Boyer <fabrice.boyer@easymile.com>
2022-11-17 08:30:20 +0100

    mkdir

--- download_data.sh
+++ download_data.sh
@@ -2,7 +2,9 @@
 
 # Common utils
 wget_cmd="wget -q --show-progress --limit-rate=10M"
+
 out_dir=$(dirname -- "$0")/dump
+mkdir $out_dir
 
 # Arxiv Metadata
 $wget_cmd https://storage.googleapis.com/arxiv-dataset/metadata-v5/arxiv-metadata-oai.json -O $out_dir/arxiv-metadata-oai.json
