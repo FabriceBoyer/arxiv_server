4b5b08d17fa6c7f33755fcc10624f1e1d2d3d18a
Fabrice <fabrice.boyer@gmail.com>
2023-01-04 21:42:08 +0000

    mkdir -p

--- download_data.sh
+++ download_data.sh
@@ -5,7 +5,7 @@ wget_cmd="wget -q --show-progress --limit-rate=1000M"
 
 source $(dirname -- "$0")/.env
 out_dir=$DUMP_PATH
-mkdir $out_dir
+mkdir -p $out_dir
 
 # Arxiv Metadata
 # old_version : $wget_cmd https://storage.googleapis.com/arxiv-dataset/metadata-v5/arxiv-metadata-oai.json -O $out_dir/arxiv-metadata-oai.json
